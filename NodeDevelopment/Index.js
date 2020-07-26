c9f6db09c374f465c298c5bab8a8c3abfd1e8262
Gabriel Scalese <cc19171@g.unicamp.br>
2020-07-26 14:28:12 -0300

    Inserted delete method

--- NodeDevelopment/Index.js
+++ NodeDevelopment/Index.js
@@ -1,48 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = 3000;
-const connStr = "Server=regulus.cotuca.unicamp.br;Database=SKYLOS;User Id=SKYLOS;Password=skylos;";
+const connStr =
+  "Server=regulus.cotuca.unicamp.br;Database=SKYLOS;User Id=SKYLOS;Password=skylos;";
 const sql = require("mssql");
 
-sql.connect(connStr)
-    .then(conn=> global.conn = conn)
-    .catch(err => console.log("erro: " + err))
+sql
+  .connect(connStr)
+  .then((conn) => (global.conn = conn))
+  .catch((err) => console.log("erro: " + err));
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const router = express.Router();
-router.get('/',(req,res) => res.json({"mensagem": "A API está ativa!"}));
-app.use('/',router);
+router.get("/", (req, res) => res.json({ mensagem: "A API está ativa!" }));
+app.use("/", router);
 
 app.listen(port);
-console.log('O servidor está ativo!');
+console.log("O servidor está ativo!");
 
-function execSQL(conn,sqlQry,res)
-{
-    conn.request()
+function execSQL(conn, sqlQry, res) {
+  conn
+    .request()
     .query(sqlQry)
-    .then(result => console.log("Sucesso " + result))
-    .catch(err => console.log("Erro: " + err))
+    .then((result) => console.log("Sucesso " + result))
+    .catch((err) => console.log("Erro: " + err));
 }
 
+router.post("/resultados", (req, res) => {
+  const RA = parseInt(req.body.ra);
+  const Cod = parseInt(req.body.cod);
+  const Nota = parseFloat(req.body.nota);
+  const Frequencia = parseFloat(req.body.freq);
 
-router.post('/resultados',(req,res)=>
-{
-    const RA = parseInt(req.body.ra);
-    const Cod =  parseInt(req.body.cod);
-    const Nota = parseFloat(req.body.nota);
-    const Frequencia = parseFloat(req.body.freq);
+  execSQL(
+    global.conn,
+    "INSERT INTO RESULTADOS(RA,COD,NOTA,FREQUENCIA) VALUES(" +
+      RA +
+      "," +
+      Cod +
+      "," +
+      Nota +
+      "," +
+      Frequencia +
+      ")",
+    res
+  );
 
-    execSQL(global.conn,"INSERT INTO RESULTADOS(RA,COD,NOTA,FREQUENCIA) VALUES("+RA + ","+Cod+","+Nota+","+Frequencia+")",res)
+  execSQL(global.conn, "DELETE FROM MATRICULAS WHERE RA=" + RA);
 
-    return res.json();
-})
-
-
-router.delete('/matriculas',(req,res)=>
-{
-
-})
-
+  return res.json();
+});
