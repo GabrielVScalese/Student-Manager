173535b6dacd793581be8b7e3819d8a8988e11c1
Gabriel Scalese <cc19171@g.unicamp.br>
2020-07-26 20:57:51 -0300

    Updated code

--- MSSQLDevelopment/ConsultaDeDados.sql
+++ MSSQLDevelopment/ConsultaDeDados.sql
@@ -1,14 +1,49 @@
+/* Tabela Alunos */
+
+select * from Alunos
+insert into Alunos values (19171,'Gabriel')
+insert into Alunos values (19199,'Vicente')
+insert into Alunos values (19200,'Vitor')
+sp_help Alunos
+
+/* Tabela Resultados */
+
 select * from Resultados
-
-drop table Resultados
-
 create table Resultados
 (
-RA smallint,
+RA int,
 Cod int,
 Nota float not null,
 Frequencia float not null,
 primary key(RA,Cod)
 )
-
+drop table Resultados
 sp_help Resultados
+
+delete from Resultados where RA = 
+
+/* Tabela Matriculas */
+
+select * from Matriculas
+insert into Matriculas values (19171, 1)
+insert into Matriculas values (19199, 2)
+insert into Matriculas values (19200, 3)
+drop table Matriculas
+create table Matriculas 
+(
+RA smallint,
+Cod int,
+primary key(RA,Cod)
+)
+
+/* Tabela Disciplinas */
+
+select * from Disciplinas
+insert into Disciplinas values (1, 'Matemática')
+insert into Disciplinas values (2, 'Português')
+insert into Disciplinas values (3, 'Artes')
+
+
+
+
+
