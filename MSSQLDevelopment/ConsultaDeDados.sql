ad5fb21c442e6db40fc6d837819ef8df3e826d2e
unknown <vincentius.ptj@gmail.com>
2020-07-27 13:29:33 -0300

    Updated

--- MSSQLDevelopment/ConsultaDeDados.sql
+++ MSSQLDevelopment/ConsultaDeDados.sql
@@ -1,5 +1,9 @@
 /* Tabela Alunos */
 
+CREATE TABLE Alunos (			
+RA SMALLINT PRIMARY KEY,		
+Nome VARCHAR(40) NOT NULL)
+
 select * from Alunos
 insert into Alunos values (19171,'Gabriel')
 insert into Alunos values (19199,'Vicente')
@@ -9,13 +13,16 @@ sp_help Alunos
 /* Tabela Resultados */
 
 select * from Resultados
-create table Resultados
-(
-RA int,
-Cod int,
-Nota float not null,
-Frequencia float not null,
-primary key(RA,Cod)
+CREATE TABLE Resultados (
+RA SMALLINT,					
+Cod INT,						
+Nota FLOAT NOT NULL,
+Frequencia FLOAT NOT NULL,
+PRIMARY KEY (RA,Cod),
+FOREIGN KEY (RA)
+REFERENCES Alunos(RA),
+FOREIGN KEY (Cod)
+REFERENCES Disciplinas(Cod)
 )
 drop table Resultados
 sp_help Resultados
@@ -29,14 +36,21 @@ insert into Matriculas values (19171, 1)
 insert into Matriculas values (19199, 2)
 insert into Matriculas values (19200, 3)
 drop table Matriculas
-create table Matriculas 
-(
-RA smallint,
-Cod int,
-primary key(RA,Cod)
-)
+CREATE TABLE Matriculas (		
+RA SMALLINT,					
+Cod INT,						
+PRIMARY KEY (RA,Cod),			
+FOREIGN KEY (RA)				
+REFERENCES Alunos(RA),		
+FOREIGN KEY (Cod)				
+REFERENCES Disciplinas(Cod))
 
 /* Tabela Disciplinas */
+
+CREATE TABLE Disciplinas (
+Cod INT PRIMARY KEY,
+Nome VARCHAR(40) NOT NULL)
+
 
 select * from Disciplinas
 insert into Disciplinas values (1, 'Matemática')
