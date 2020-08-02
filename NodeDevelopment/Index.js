be8f1cc4845caee957b77a9072a795cc37158e08
Vitor Mugnol <vitormugnol10@gmail.com>
2020-08-02 15:35:03 -0300

    bug fix

--- NodeDevelopment/Index.js
+++ NodeDevelopment/Index.js
@@ -21,9 +21,9 @@ app.use("/", router);
 app.listen(port);
 console.log("O servidor está ativo!");
 
-async function execSQL(sqlQry) {
+async function execSQL(sqlQry, conn) {
   try {
-    return await global.conn.request().query(sqlQry);
+    return await conn.request().query(sqlQry);
   } catch (err) {
     console.log("Erro: " + err);
   }
@@ -35,13 +35,17 @@ router.post("/resultados", async (req, res) => {
   const Nota = parseFloat(req.body.nota);
   const Frequencia = parseFloat(req.body.freq);
 
-  const existsAluno = await execSQL("SELECT * FROM ALUNOS WHERE RA =" + RA);
+  const existsAluno = await execSQL("SELECT * FROM ALUNOS WHERE RA =" + RA, global.conn);
 
   const existsDisciplina = await execSQL(
-    "SELECT * FROM DISCIPLINAS WHERE COD=" + Cod
+    "SELECT * FROM DISCIPLINAS WHERE COD=" + Cod, global.conn
   );
   const existsMatricula = await execSQL(
-    "SELECT * FROM MATRICULAS WHERE RA=" + RA + "AND COD=" + Cod
+    "SELECT * FROM MATRICULAS WHERE RA=" + RA + "AND COD=" + Cod, global.conn
+  );
+
+  const existsResultados = await execSQL(
+    "SELECT * FROM RESULTADOS WHERE RA=" + RA + "AND COD=" + Cod, global.conn
   );
 
   if (existsAluno.recordset.length == 0) {
@@ -50,9 +54,10 @@ router.post("/resultados", async (req, res) => {
     return res.json(404);
   } else if (existsMatricula.recordset.length == 0) {
     return res.json(404);
+  } else if (existsResultados.recordset.length != 0) {
+    return res.json(404);
   } else {
     execSQL(
-      global.conn,
       "INSERT INTO RESULTADOS(RA,COD,NOTA,FREQUENCIA) VALUES(" +
         RA +
         "," +
@@ -61,10 +66,11 @@ router.post("/resultados", async (req, res) => {
         Nota +
         "," +
         Frequencia +
-        ")"
+        ")",
+      global.conn
     );
 
-    execSQL("DELETE FROM MATRICULAS WHERE RA=" + RA + "AND COD=" + Cod);
+    execSQL("DELETE FROM MATRICULAS WHERE RA=" + RA + "AND COD=" + Cod, global.conn);
 
     return res.json(200);
   }
